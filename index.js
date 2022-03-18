3c09816c872addf313e65955ec0e45d353d0597b
javierdebug <99869456+javierdebug@users.noreply.github.com>
2022-03-18 17:33:19 -0500

    Improve the code
    Eliminate the use of jQuery library, so now the app works with vanilla Javascript code.

--- index.js
+++ index.js
@@ -1,91 +1,96 @@
-    let tasks = [];
-    let tasksString = "";
-    let done = [];
-    let doneString = "";
-    let inputVal = "";
-    let taskDone = "";
-    initiate();
+let tasks = [];
+let tasksString = "";
+let done = [];
+let doneString = "";
+let inputVal = "";
+let taskDone = "";
+initiate();
+
+function initiate() {
+        if (localStorage.getItem("tasks")) {
+            tasksString = localStorage.getItem("tasks");
+            tasks = JSON.parse(tasksString);
+        }
     
-    function initiate() {
-            if (localStorage.getItem("tasks")) {
-                tasksString = localStorage.getItem("tasks");
-                tasks = JSON.parse(tasksString);
-            }
+        if (localStorage.getItem("done")) {
+            doneString = localStorage.getItem("done");
+            done = JSON.parse(doneString);
+        }  
         
-            if (localStorage.getItem("done")) {
-                doneString = localStorage.getItem("done");
-                done = JSON.parse(doneString);
-            }  
-            
-            for (let i = 0; i < tasks.length; i++) {
-                $("#tasks").append("<li>" + tasks[i] +
-                ` <button class="btnDoneCurrent">✓</button>` + 
-                ` <button class="btnRemoveCurrent">X</button>` +
-                `</li>`);
-            }
-        
-            done.forEach(el => {
-                $("#done").append("<li>" + el + 
-                ` <button class="btnRevertCurrent">↺</button>` +
-                "</li>");
-            });
-    }
-
-    $("#btn-add").on("click", function(){
-        inputVal = $("#inputTask").val();
-        
-        if (inputVal == "") {
-            alert('Enter a task');
-        } else {
-            tasks.push(inputVal);
-            saveCurrentTasksToLocal();
-
-            $("#tasks").append("<li>" + inputVal +
+        for (let i = 0; i < tasks.length; i++) {
+            document.querySelector("#tasks").append("<li>" + tasks[i] +
             ` <button class="btnDoneCurrent">✓</button>` + 
             ` <button class="btnRemoveCurrent">X</button>` +
             `</li>`);
         }
-        $("#inputTask").val(""); //Clear the current added task from the input.       
-    });
+    
+        done.forEach(el => {
+            document.querySelector("#done").insertAdjacentHTML("beforeend","<li>" + el + 
+            ` <button class="btnRevertCurrent">↺</button>` +
+            "</li>");
+        });
+}
 
-    $("#inputTask").keydown(function (e) { 
-        if (e.which == 13) {
-            $("#btn-add").click();
-        }
-    });
-
-    $("body").on("click",".btnRemoveCurrent", function(e){
-        let currentTask = $(this).closest("li").text().match(/[\W\s\w]+(?= \W)/m).join(); //Regex to select the task name (every char, symbol and/or spaces) just before button
-        let index = tasks.indexOf(currentTask);
-        tasks.splice(index,1);
-
+document.querySelector("#btn-add").addEventListener("click", function(){
+    inputVal = document.querySelector("#inputTask").value;
+    
+    if (inputVal == "") {
+        alert('Enter a task');
+    } else {
+        tasks.push(inputVal);
         saveCurrentTasksToLocal();
 
-        $(this).closest("li").remove();
-    });
+        document.querySelector("#tasks").insertAdjacentHTML("beforeend","<li>" + inputVal +
+        ` <button class="btnDoneCurrent">✓</button>` + 
+        ` <button class="btnRemoveCurrent">X</button>` +
+        `</li>`);
+    }
+    document.querySelector("#inputTask").value = ""; //Clear the current added task from the input.       
+});
+
+document.querySelector("#inputTask").addEventListener("keydown", function (e) { 
+    if (e.which == 13) {
+        document.querySelector("#btn-add").click();
+    }
+});
+
+document.querySelector('body').addEventListener('click', function(event) { //An eventListener added to the body but that get executed for specific button's className
+
+    if (event.target.className === 'btnRemoveCurrent') {
+        console.log(event.target.closest("li").innerText);
+        let currentTask = event.target.closest("li").innerText.match(/[\W\s\w]+(?= \W)/m).join(); //Regex to select the task name (every char, symbol and/or spaces) just before button
+        let index = tasks.indexOf(currentTask);
+        tasks.splice(index,1);
     
-    $("body").on("click",".btnDoneCurrent", function () {
-       
-        taskDone = $(this).closest("li").text().match(/[\W\s\w]+(?= \W)/m).join(); //Regex to select the task name (every char, symbol and/or spaces) just before button
+        saveCurrentTasksToLocal();
+    
+        event.target.closest("li").remove();
+    }
+
+    if (event.target.className === "btnDoneCurrent") {
+        console.log(event.target.closest("li").innerText);
+        taskDone = event.target.closest("li").innerText.match(/[\W\s\w]+(?= \W)/m).join(); //Regex to select the task name (every char, symbol and/or spaces) just before button
 
         done.push(taskDone);
         saveDoneTasksToLocal();
+
         let index = tasks.indexOf(taskDone);
         tasks.splice(index,1);
+
         saveCurrentTasksToLocal();
         
-        $(this).closest("li").remove();
+        event.target.closest("li").remove();
         checkDoneTasks();
-    })
+    }
 
-    $("body").on("click", ".btnRevertCurrent", function () {
-    
-        inputVal = $(this).closest("li").text().match(/[\W\s\w]+(?= \W)/m).join(); //Regex to select the task name (every char, symbol and/or spaces) just before button
+    if (event.target.className === "btnRevertCurrent") {
+        
+        inputVal = event.target.closest("li").innerText.match(/[\W\s\w]+(?= \W)/m).join(); //Regex to select the task name (every char, symbol and/or spaces) just before button
 
         tasks.push(inputVal);
         saveCurrentTasksToLocal();
         
-        $("#tasks").append("<li>" + inputVal +
+        document.querySelector("#tasks").insertAdjacentHTML("beforeend","<li>" + inputVal +
         ` <button class="btnDoneCurrent">✓</button>` + 
         ` <button class="btnRemoveCurrent">X</button>` +
         `</li>`);
@@ -95,35 +100,36 @@
         done.splice(index,1);
         saveDoneTasksToLocal();
 
-        $(this).closest("li").remove();
+        event.target.closest("li").remove();
         checkDoneTasks();
+    }
+
+});
+
+function checkDoneTasks() {
+    document.querySelector("#done").innerHTML = ""; // Remove the current tasks, to avoid overwritting content.
+
+    done.forEach(el => {
+        document.querySelector("#done").insertAdjacentHTML("beforeend","<li>" + el + 
+        ` <button class="btnRevertCurrent">↺</button>` +
+        "</li>");
     });
-    
-    function checkDoneTasks() {
-        $("#done").html(""); // Remove the current tasks, to avoid overwritting content.
+}
 
-        done.forEach(el => {
-            $("#done").append("<li>" + el + 
-            ` <button class="btnRevertCurrent">↺</button>` +
-            "</li>");
-        });
-        
-    }
+function saveCurrentTasksToLocal() {
+    tasksString = JSON.stringify(tasks);
+    localStorage.setItem(`tasks`, tasksString);
+}
 
-    function saveCurrentTasksToLocal() {
-        tasksString = JSON.stringify(tasks);
-        localStorage.setItem(`tasks`, tasksString);
-    }
+function saveDoneTasksToLocal() {
+    doneString = JSON.stringify(done);
+    localStorage.setItem(`done`, doneString);
+}
 
-    function saveDoneTasksToLocal() {
-        doneString = JSON.stringify(done);
-        localStorage.setItem(`done`, doneString);
-    }
-
-    $("#btn-clearAll").on("click", function (e) {
-        done = [];
-        $("#done").html("");
-        tasks = [];
-        $("#tasks").html("");
-        localStorage.clear();   
-    });
+document.querySelector("#btn-clearAll").addEventListener("click", function (e) {
+    done = [];
+    document.querySelector("#done").innerHTML = "";
+    tasks = [];
+    document.querySelector("#tasks").innerHTML = "";
+    localStorage.clear();   
+});
